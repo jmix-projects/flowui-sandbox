9361170e52aa974b403bba5c0190c24850be5729
Gleb Gorelov <g.gorelov@haulmont.com>
2022-06-15 17:39:11 +0400

    Use optionsContainer attribute

--- src/main/java/com/company/demo/view/orderitem/OrderItemDetail.java
+++ src/main/java/com/company/demo/view/orderitem/OrderItemDetail.java
@@ -1,14 +1,13 @@
 package com.company.demo.view.orderitem;
 
 import com.company.demo.entity.OrderItem;
-import com.company.demo.entity.Product;
 import com.company.demo.view.main.MainView;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RoutePrefix;
-import io.jmix.flowui.component.combobox.EntityComboBox;
-import io.jmix.flowui.data.options.ContainerOptions;
-import io.jmix.flowui.model.CollectionContainer;
-import io.jmix.flowui.screen.*;
+import io.jmix.flowui.screen.EditedEntityContainer;
+import io.jmix.flowui.screen.StandardEditor;
+import io.jmix.flowui.screen.UiController;
+import io.jmix.flowui.screen.UiDescriptor;
 
 @Route(value = ":id", layout = MainView.class)
 @RoutePrefix("order-items")
@@ -16,15 +15,4 @@ import io.jmix.flowui.screen.*;
 @UiDescriptor("order-item-detail.xml")
 @EditedEntityContainer("orderItemDc")
 public class OrderItemDetail extends StandardEditor<OrderItem> {
-
-    @ComponentId
-    private EntityComboBox<Product> productField;
-
-    @ComponentId
-    private CollectionContainer<Product> productsDc;
-
-    @Subscribe
-    public void onInit(InitEvent event) {
-        productField.setListOptions(new ContainerOptions<>(productsDc));
-    }
 }
