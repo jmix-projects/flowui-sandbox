2b7856cf09278066eb1b67a2a091bb2d4106583e
Gleb Gorelov <g.gorelov@haulmont.com>
2022-08-23 17:41:33 +0400

    Update according to the latest changes

--- src/main/java/com/company/demo/view/orderitem/OrderItemDetail.java
+++ src/main/java/com/company/demo/view/orderitem/OrderItemDetail.java
@@ -3,14 +3,12 @@ package com.company.demo.view.orderitem;
 import com.company.demo.entity.OrderItem;
 import com.company.demo.view.main.MainView;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RoutePrefix;
 import io.jmix.flowui.view.EditedEntityContainer;
 import io.jmix.flowui.view.StandardDetailView;
 import io.jmix.flowui.view.UiController;
 import io.jmix.flowui.view.UiDescriptor;
 
-@Route(value = ":id", layout = MainView.class)
-@RoutePrefix("order-items")
+@Route(value = "order-items/:id", layout = MainView.class)
 @UiController("OrderItem.detail")
 @UiDescriptor("order-item-detail-view.xml")
 @EditedEntityContainer("orderItemDc")
--- src/main/java/com/company/demo/view/user/UserListView.java
+++ src/main/java/com/company/demo/view/user/UserListView.java
@@ -5,12 +5,10 @@ import com.company.demo.view.main.MainView;
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.view.*;
 
-@Route(value = UserListView.ROUTE, layout = MainView.class)
+@Route(value = "users", layout = MainView.class)
 @UiController("User.list")
 @UiDescriptor("user-list-view.xml")
 @LookupComponent("usersTable")
 @DialogMode(width = "50em", height = "37.5em")
 public class UserListView extends StandardListView<User> {
-
-    public static final String ROUTE = "users";
 }
